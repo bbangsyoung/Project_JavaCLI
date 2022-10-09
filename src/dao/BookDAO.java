706f1fa489f2767a731906c9273c9abf43021fba
bbangdi <bbangord@gmail.com>
2022-10-09 19:12:11 +0900

    도서대여 기능 DAO 추가

--- src/dao/BookDAO.java
+++ src/dao/BookDAO.java
@@ -100,6 +100,32 @@ public class BookDAO {
         return book;
     }
 
+    public Book selectBookRental(int book_no) {
+        Book book = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        String sql = "SELECT * FROM book where book_no = ?";
+
+        try{
+            pstmt = con.prepareStatement(sql);
+            pstmt.setInt(1, book_no);
+            rs = pstmt.executeQuery();
+
+            if(rs.next()) {
+                book = new Book(rs.getBoolean("book_rental"));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally{
+            close(rs);
+            close(pstmt);
+        }
+        return book;
+    }
+
+
+
     public ArrayList<Book> selectBookByBook_no(int book_no) {
         ArrayList<Book> searchBookList = null;
         Book searchBook = null;
@@ -172,6 +198,43 @@ public ArrayList<Book> selectBookListByBook_name(String book_name) {
 }
 
 
+
+    public ArrayList<Book> selectBookListByBook_rental(int book_no) {
+        ArrayList<Book> searchBookList = null;
+        Book searchBook = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        String sql = "SELECT * FROM book WHERE book_no like concat(?)";
+
+        try{
+            pstmt = con.prepareStatement(sql);
+
+            pstmt.setInt(1, book_no);
+            rs = pstmt.executeQuery();
+
+            if(rs.next()) {
+                searchBookList = new ArrayList<Book>();
+
+                do{
+                    searchBook = new Book(
+                            rs.getBoolean("book_rental"));
+
+                    searchBookList.add(searchBook);
+                } while(rs.next());
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            close(rs);
+            close(pstmt);
+        }
+        return searchBookList;
+    }
+
+
+
+
     public ArrayList<Book> selectBookListByBook_writer(String book_writer) {
         ArrayList<Book> searchBookList = null;
         Book searchBook = null;
@@ -230,6 +293,32 @@ public int deleteBook(int book_no) {
 
 
     public int updateBook(Book changeBook) throws Exception{
+
+        int updateCount = 0;
+        PreparedStatement pstmt = null;
+        String sql = "UPDATE book SET book_name=?, book_writer=? WHERE book_no=?;";
+
+        try{
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, changeBook.getBook_name());
+            pstmt.setString(2, changeBook.getBook_writer());
+            pstmt.setInt(3, changeBook.getBook_no());
+
+            updateCount = pstmt.executeUpdate();
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+        finally{
+            close(pstmt);
+        }
+
+        return updateCount;
+
+    }
+
+
+    public int updateBookReantal(Book changeBook) throws Exception{
 
         int updateCount = 0;
         PreparedStatement pstmt = null;
